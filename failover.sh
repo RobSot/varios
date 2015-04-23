df48a39df5701fb6fe089babb9dcb4615ccf9ce6
Roberto Rodriguez Soto <roberdw@gmail.com>
2015-04-23 23:14:01 +0200

    Sudo support

--- failover.sh
+++ failover.sh
@@ -9,23 +9,23 @@ while sleep $TIME
 do
         if [ "$USING_3G" = "yes" ]; then
 		if [ "`ip r list | grep default | awk '{print $5}'`" = "eth0" ]; then
-			ip r delete default via 192.168.159.10 dev eth0
-			ip r add default via 192.168.43.1 dev wlan0
+			sudo ip r delete default via 192.168.159.10 dev eth0
+			sudo ip r add default via 192.168.43.1 dev wlan0
                 fi
                 if ping -q -c 1 -W $TIMEOUT -I wlan0 $ICMP > /dev/null; then
                        	echo "wlan0 net OK"
                	else
                        	echo "wlan0 net Fail 1"
                        	echo "Activando ethernet net..."
-			ip r delete default via 192.168.43.1 dev wlan0
-                       	ip r add default via 192.168.159.10 dev eth0
+			sudo ip r delete default via 192.168.43.1 dev wlan0
+                       	sudo ip r add default via 192.168.159.10 dev eth0
                        	USING_3G="no"
                 fi
         else	# Usando ethernet (o arranque del script)
         	while [ "`ip r list | grep default | awk '{print $5}'`" != "wlan0" ];		# Espera mientras el 3G no está activo
 		do
 			if [ "`ip r list | grep default | awk '{print $5}'`" = "" ]; then	# Revisa que eth0 tenga ruta si está presente
-				ip r add default via 192.168.159.10 dev eth0			# Ojo que no pise al 3G cuando se active, revisar
+				sudo ip r add default via 192.168.159.10 dev eth0			# Ojo que no pise al 3G cuando se active, revisar
 			fi
 			echo "waiting for wlan0 up"
 			sleep 1
@@ -36,8 +36,8 @@ do
                 else
                 	echo "wlan0 net Fail 2"
                        	echo "Activando ethernet net..."
-			ip r delete default via 192.168.43.1 dev wlan0
-                       	ip r add default via 192.168.159.10 dev eth0
+			sudo ip r delete default via 192.168.43.1 dev wlan0
+                       	sudo ip r add default via 192.168.159.10 dev eth0
                 fi
         fi
 done;
